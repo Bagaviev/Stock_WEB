037eacbe9f5de39118324be06332a445287ed453
bagavievbulat <buba1219@yandex.ru>
2020-06-24 23:39:20 +0300

    Privet, test Pycharm Github integration

--- manage.py
+++ manage.py
@@ -1,12 +1,11 @@
+#-*- coding: UTF-8 -*-
 import flask
-
+from flask import render_template, flash, redirect, url_for, request
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
-
-print("ebal rti")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
