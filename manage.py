23cbf70631e16c5c933c2a69497a8f9cd6463802
bagavievbulat <buba1219@yandex.ru>
2020-06-26 21:52:30 +0300

    Privet, test Pycharm Github integration

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ from flask import render_template, flash, redirect, url_for, request
 app = flask.Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def index():
+    print(url_for('index'))
     return render_template('index.html')
 
 if __name__ == "__main__":
