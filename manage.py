58c896f6f3f6d00862b659a72a0cb2198c274b91
bagavievbulat <buba1219@yandex.ru>
2020-06-27 21:17:45 +0300

    Privet, test Pycharm Github integration

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #-*- coding: UTF-8 -*-
 import flask
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, url_for
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,21 @@ app = flask.Flask(__name__)
 def index():
     print(url_for('index'))
     return render_template('index.html')
+
+@app.route('/month.html')
+def month():
+    print(url_for('month'))
+    return render_template('month.html')
+
+@app.route('/week.html')
+def week():
+    print(url_for('week'))
+    return render_template('week.html')
+
+@app.route('/patterns.html')
+def patterns():
+    print(url_for('patterns'))
+    return render_template('patterns.html')
 
 if __name__ == "__main__":
     app.run()
