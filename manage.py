fff335406b36026cea93115bf18d793c6eaecbc5
bagavievbulat <buba1219@yandex.ru>
2020-06-27 23:33:16 +0300

    Privet, test Pycharm Github integration

--- manage.py
+++ manage.py
@@ -1,28 +1,33 @@
 #-*- coding: UTF-8 -*-
-import flask
+import flask, pandas
 from flask import render_template, url_for
+
+data = pandas.read_csv('static\data\parsed_EURUSD_y.csv', delimiter=';')
+act = data.is_actual.tail(1).to_string(index=False)
+cls = data.last_CLOSE.tail(1).to_string(index=False)
+
 app = flask.Flask(__name__)
 
 @app.route('/')
 @app.route('/index.html')
 def index():
     print(url_for('index'))
-    return render_template('index.html')
+    return render_template('index.html', act=act, cls=cls)
 
 @app.route('/month.html')
 def month():
     print(url_for('month'))
-    return render_template('month.html')
+    return render_template('month.html', act=act, cls=cls)
 
 @app.route('/week.html')
 def week():
     print(url_for('week'))
-    return render_template('week.html')
+    return render_template('week.html', act=act, cls=cls)
 
 @app.route('/patterns.html')
 def patterns():
     print(url_for('patterns'))
-    return render_template('patterns.html')
+    return render_template('patterns.html', act=act, cls=cls)
 
 if __name__ == "__main__":
     app.run()
